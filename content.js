2225f22becf8cff14b57181e8301c2737b15ab69
Zarigani <zari220130@gmail.com>
2022-11-04 15:30:08 +0900

    General update

--- content.js
+++
@@ -1,9 +0,0 @@
-// Send injection request on parent loaded
-const initInjectionInterval = setInterval(() => {
-  const parent = document.querySelector("#description-inline-expander");
-
-  if (parent) {
-    chrome.runtime.sendMessage({ command: "injectionReq" });
-    clearInterval(initInjectionInterval);
-  }
-}, 500);
---
+++ webpack.config.prod.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const path = require("path");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+const WebpackBar = require("webpackbar");
+const CopyPlugin = require("copy-webpack-plugin");
+
+/**@type {import('webpack').Configuration} */
+const commonConfig = {
+  name: "common",
+  mode: "production",
+  stats: "errors-only",
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/i,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  plugins: [
+    new FriendlyErrorsWebpackPlugin({ clearConsole: false }),
+    new WebpackBar(),
+    new CopyPlugin({
+      patterns: [{ from: "public", to: "." }],
+    }),
+  ],
+};
+
+/**@type {import('webpack').Configuration} */
+const content = {
+  name: "content",
+  target: "web",
+  entry: "./src/content/script/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "content.js",
+    clean: true,
+  },
+  ...commonConfig,
+};
+
+/**@type {import('webpack').Configuration} */
+const background = {
+  name: "background",
+  target: "web",
+  entry: "./src/background/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "background.js",
+  },
+  ...commonConfig,
+};
+
+module.exports = [content, background];
